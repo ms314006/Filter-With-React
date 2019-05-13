e6b2c835e837e779b383577185658d054e25da28
GQSM <ms314006@hotmail.com>
2019-05-13 22:47:41 +0800

    [ADD] 增加 antd 的頁數區塊

--- src/component/Content/DataList.js
+++ src/component/Content/DataList.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { Pagination } from 'antd';
+import { fetchData } from '../../actions/filter.js';
 import styles from './index.scss';
 
-const DataList = () => {
+const DataList = (props) => {
+  const { dataList, } = props;
+  useEffect(() => {
+    props.fetchData();
+  }, []);
+
   return (
     <div className={styles.content_data_block}>
-      dsfsdfdsfdsfd
+      <Pagination
+        defaultCurrent={1}
+        total={dataList.length}
+        onChange={(page, pageSize) => { console.log(page); }}
+      />
     </div>
   );
 };
 
-export default DataList;
+const mapStateToProps = state => ({
+  dataList: state.dataList,
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchData: fetchData(dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(DataList);
