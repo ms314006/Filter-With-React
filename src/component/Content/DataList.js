22d34d67443a14427eb9817f3eaad600e944a4d9
GQSM <ms314006@hotmail.com>
2019-05-14 07:42:43 +0800

    [ADD] 介接用頁數篩選的資料和 action

--- src/component/Content/DataList.js
+++ src/component/Content/DataList.js
@@ -1,32 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Pagination } from 'antd';
-import { fetchData } from '../../actions/filter.js';
+import { fetchData, filterData } from '../../actions/filter.js';
 import styles from './index.scss';
 
 const DataList = (props) => {
-  const { dataList, } = props;
+  const { originDataList, filterDataList, } = props;
   useEffect(() => {
     props.fetchData();
   }, []);
-
   return (
     <div className={styles.content_data_block}>
       <Pagination
         defaultCurrent={1}
-        total={dataList.length}
-        onChange={(page, pageSize) => { console.log(page); }}
+        total={originDataList.length}
+        onChange={(page, pageSize) => { props.filterData({ page, pageSize, }); }}
       />
+      {
+        filterDataList.map(data => <h1 key={data.Id}>{data.Name}</h1>)
+      }
     </div>
   );
 };
 
 const mapStateToProps = state => ({
-  dataList: state.dataList,
+  originDataList: state.originDataList,
+  filterDataList: state.filterDataList,
 });
 
 const mapDispatchToProps = dispatch => ({
   fetchData: fetchData(dispatch),
+  filterData: query => dispatch(filterData(query)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(DataList);
