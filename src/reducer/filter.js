e6284684261897b4dff99f109507a9ad542e18e2
GQSM <ms314006@hotmail.com>
2019-05-14 22:24:00 +0800

    [ADD] 將篩選條件的部分加上去

--- src/reducer/filter.js
+++ src/reducer/filter.js
@@ -2,6 +2,8 @@ import { CHANGE_SEARCH_QUERY, FETCH_DATA, FILTER_DATA } from '../actions/filter.
 
 const initialState = {
   searchQuery: {
+    page: 1,
+    pageSize: 10,
     keyWord: '',
     free: '',
     allDayOpen: false,
@@ -12,19 +14,38 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case CHANGE_SEARCH_QUERY:
+    case CHANGE_SEARCH_QUERY: {
+      const newQuery = {
+        ...state.searchQuery,
+        ...action.payload,
+      };
+      let filterDataList = [...state.originDataList];
+      if (newQuery.keyWord !== '') {
+        filterDataList = filterDataList.filter(data => data.Name.indexOf(newQuery.keyWord) > -1);
+      }
+
+      if (newQuery.free === 'Y') {
+        filterDataList = filterDataList.filter(data => data.Ticketinfo === '免費參觀' || data.Ticketinfo === '');
+      } else if (newQuery.free === 'N') {
+        filterDataList = filterDataList.filter(data => data.Ticketinfo !== '免費參觀' && data.Ticketinfo !== '');
+      }
+      
+      if (newQuery.allDayOpen) {
+        filterDataList = filterDataList.filter(data => data.Opentime === '全天候開放');
+      }
       return {
         ...state,
         searchQuery: {
-          ...state.searchQuery,
-          ...action.payload,
+          ...newQuery,
         },
+        filterDataList,
       };
+    }
     case FETCH_DATA:
       return {
         ...state,
         originDataList: action.payload.originDataList,
-        filterDataList: action.payload.originDataList.slice(0, 10),
+        filterDataList: action.payload.originDataList,
       };
     case FILTER_DATA: {
       const { page, pageSize, } = action.payload.query;
