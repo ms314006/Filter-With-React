873c3d572c73800cf5538e919c447419bd44a8a7
GQSM <ms314006@hotmail.com>
2019-05-15 22:21:39 +0800

    [CHANGE] Route 換頁的地方只有右半邊的 DataList 而已，當時沒注意到

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
 import Header from '../Header';
 import Content from '../Content';
-import Information from '../Information';
 import styles from './index.scss';
 
-const MainPage = () => (
+const Main = () => (
   <div className={styles.main_block}>
     <Header />
     <Content />
   </div>
-);
-
-const Main = () => (
-  <>
-    <Route exact path="/" component={MainPage} />
-    <Route exact path="/informaction/:Id" component={Information} />
-  </>
 );
 
 export default Main;
