a26f27c5bb8e2706847b71870558e20d321d1088
GQSM <ms314006@hotmail.com>
2019-05-15 22:19:48 +0800

    [CHANGE] 移除在 index.js 的 HasgRouter ，將他移到有 Route 的 Content ，然後載入資料也改成從 Content 中處理

--- src/component/Content/Content.js
+++ src/component/Content/Content.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import SideSearchBlock from './SideSearchBlock';
 import DataList from './DataList';
+import Information from '../Information';
+import { fetchData } from '../../actions/filter.js';
 
-const Content = () => (
-  <>
-    <SideSearchBlock />
-    <DataList />
-  </>
-);
+const Content = (props) => {
+  useEffect(() => {
+    props.fetchData();
+  }, []);
+  return (
+    <>
+      <SideSearchBlock />
+      <HashRouter>
+        <Switch>
+          <Route exact path="/" component={DataList} />
+          <Route path="/informaction/:Id" component={Information} />
+        </Switch>
+      </HashRouter>
+    </>
+  );
+};
 
-export default Content;
+const mapDispatchToState = dispatch => ({
+  fetchData: fetchData(dispatch),
+});
+
+export default connect(null, mapDispatchToState)(Content);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
 import store from './store';
 import Main from './component/Main';
 
 ReactDOM.render(
   <Provider store={store}>
-    <HashRouter>
-      <Main />
-    </HashRouter>
+    <Main />
   </Provider>,
   document.getElementById('root')
 );
